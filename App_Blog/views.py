794bb34bc79e0dccd4ef0f28f66d7e4194a30b2d
erictumuti <erictumuti114@gmail.com>
2020-09-28 18:27:06 +0300

    individual blog page posts and being able to edit each post

--- App_Blog/views.py
+++ App_Blog/views.py
@@ -8,6 +8,19 @@ import uuid
 from App_Blog.forms import CommentForm
 
 # Create your views here.
+
+
+class MyBlogs(LoginRequiredMixin, TemplateView):
+    template_name = 'App_Blog/my_blogs.html'
+
+class UpdateBlog(LoginRequiredMixin, UpdateView):
+    model = Blog
+    fields = ('blog_title', 'blog_content', 'blog_image')
+    template_name = 'App_Blog/edit_blog.html'
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('App_Blog:blog_details', kwargs={'slug':self.object.slug})
+
 def blog_list(request):
     return render(request, 'App_Blog/blog_list.html', context={})
 
